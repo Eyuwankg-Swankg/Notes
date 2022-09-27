ac429bc513af67ebb6f378bdf31eb1c57e7aaff6
Eyuwankg-Swankg <messikarthik13@gmail.com>
2022-09-27 23:15:21 +0530

    added packages

---
+++ Java/Package/FirstPackage/Skoda.java
@@ -0,0 +1,26 @@
+package Package.FirstPackage;
+
+import Package.SecondPackage.Cars;
+
+class Skoda {
+
+    public String model;
+    public int torque;
+
+    Skoda(String model, int torque) {
+        this.model = model;
+        this.torque = torque;
+    }
+
+    public static void main(String args[]) {
+
+        Cars cars = new Cars("SKODA", " Laurin & Klement", 1948, 2.34);
+
+        Skoda skoda = new Skoda("OCTAVIA VRS 230", 350);
+
+        System.out.println("Make : " + cars.make + "\n" + "Founder : " + cars.founder + "\n" + "Established Year : "
+                + cars.getYear() + "\n" + "Share Price : " + cars.getSharePrice());
+        System.out.println("Model : " + skoda.model + "\n" + "Torque : " + skoda.torque + "Nm\n");
+
+    }
+}
---
+++ Java/Package/SecondPackage/Cars.java
@@ -0,0 +1,26 @@
+package Package.SecondPackage;
+
+public class Cars {
+    public String make;
+    public String founder;
+    //private memebers are not accessible across package and only accessible inside the same class
+    private int year;
+    //protected members are not accessible across package and accessible only inside the same package
+    protected double sharePrice;
+
+    public Cars(String make, String founder, int year, double sharePrice) {
+        this.make = make;
+        this.founder = founder;
+        this.year = year;
+        this.sharePrice = sharePrice;
+
+    }
+    // getters to get protected members
+    public double getSharePrice() {
+        return this.sharePrice;
+    }
+    // getters to get private members
+    public int getYear() {
+        return this.year;
+    }
+}
