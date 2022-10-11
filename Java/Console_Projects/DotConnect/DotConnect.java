b87ab47031efd399cfca03e47ffbc9a6020cd942
Eyuwankg-Swankg <messikarthik13@gmail.com>
2022-10-12 01:04:31 +0530

    completed DotConnect project

--- Java/Console_Projects/DotConnect/DotConnect.java
+++ Java/Console_Projects/DotConnect/DotConnect.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
-public class Main
+
+
+public class DotConnect 
 {
     static int[][] board;
     static boolean playerTurn=true;//true=player1 & false=player2
@@ -21,6 +23,10 @@ public class Main
                     System.out.print("-");    
                 else if(board[i][j]==0)
                     System.out.print(" ");    
+                else if(board[i][j]==-3)
+                    System.out.print("P1");    
+                else if(board[i][j]==-4)
+                    System.out.print("P2");    
                 else
                     System.out.print(board[i][j]);
             }
@@ -73,59 +79,207 @@ public class Main
     static boolean checkBox(int direction,int rowA,int colA,int rowB,int colB){
         
         int pathCount=0;
-        
-        //top
+        boolean status=false;
         if(direction==0){
+
+            // LEFT
+
+            // <-
+            //  |
+            if(colB>1 && board[rowB][colB-1]==-2)
+                pathCount++;
             
-            // check for left
+            //  |
+            //  <-
+            if(colA>1 && rowA>0 && board[rowA-1][colA-2]==-1)
+                pathCount++;
+
+            //  <-
+            if(colA>1 && board[rowA][colA-1]==-2)
+                pathCount++;
             
-            // right
-            if(colA<boardCol-2 && ( board[rowA][colA+1]==-1 || board[rowA][colA+1]==-2)){
-                pathCount++;
+            if(pathCount>2){
+                board[rowA-1][colA-1]=playerTurn?-3:-4;
+                status=true;
             }
-            // right top
-            if(rowA>1 && ( board[rowA-1][colA+2]==-1 || board[rowA-1][colA+2]==-2)){
+            else 
+                pathCount=0;
+            
+            // RIGHT 
+
+            // ->
+            // |
+            if(colB<boardCol-2 && board[rowB][colB+1]==-2)
                 pathCount++;
-            }
-            // top right
-            if(colB<boardCol-2 && ( board[rowB][colB+1]==-1 || board[rowB][colB+1]==-2)){
+
+            //  |
+            // ->
+            if(rowA>1 && colA<boardCol-2 && board[rowA-1][colA+2]==-1)
                 pathCount++;
+
+            // ->
+            if(colA<boardCol-2 && board[rowA][colA+1]==-2)
+                pathCount++;
+
+            if(pathCount>2){
+                board[rowA-1][colA+1]=playerTurn?-3:-4;
+                status=true;
             }
         }
         
-        //right
-        if(direction==0){
+        else if(direction==2){
+
+            //bottom
+
+            // LEFT 
+
+            //   |
+            //  <-
+            if(colB>1 && board[rowB][colB-1]==-2)
+                pathCount++;
+
+            // <-
+            // |
+            if(rowA<boardRow-2 && colA>1 && board[rowA+1][colA-2]==-1)
+                pathCount++;
+
+            // <-
+            if(colA>1 && board[rowA][colA-1]==-2)
+                pathCount++;
             
-            // bottom
-            if(rowA<boardRow-2 && ( board[rowA+1][colA]==-1 || board[rowA+1][colA]==-2)){
-                pathCount++;
+            if(pathCount>2){
+                board[rowA+1][colA-1]=playerTurn?-3:-4;
+                status=true;
             }
-            // botttom right 
-            if(colA<boardCol-2 && rowA<boardRow-2 && ( board[rowA+2][colA+1]==-1 || board[rowA+2][colA+1]==-2)){
+            else 
+                pathCount=0;
+
+            // RIGHT 
+
+            // |
+            // ->
+            if(colB<boardCol-2 && board[rowB][colB+1]==-2)
                 pathCount++;
-            }
-            // right bottom
-            if(rowB<boardRow-2 && ( board[rowB+1][colB]==-1 || board[rowB+1][colB]==-2)){
-                pathCount++;
-            }
             
-            // top
-            if(rowA>1 && ( board[rowA-1][colA]==-1 || board[rowA-1][colA]==-2)){
+            // ->
+            //  |
+            if(colA<boardCol-2 && rowA<boardRow-2 && board[rowA+1][colA+2]==-1)
                 pathCount++;
-            }
-            // top right  
-            if(colA>1 && rowA<boardRow-2 && ( board[rowA+1][colA-2]==-1 || board[rowA+1][colA-2]==-2)){
+            
+            // ->
+            if(colA<boardRow-2 && board[rowA][colA+1]==-2)
                 pathCount++;
+
+            if(pathCount>2){
+                board[rowA+1][colA+1]=playerTurn?-3:-4;
+                status=true;
             }
-            // bottom left
-            if(colB>1 && ( board[rowB-1][colB]==-1 || board[rowB-1][colB]==-2)){
-                pathCount++;
-            }
+
         }
+
+        else if(direction==1){
+
+            // right 
+            // BOTTOM 
+
+            // ->
+            //  |
+            if(rowB<boardRow-2 && board[rowB+1][colB]==-1)
+                pathCount++;
+            
+            // |
+            // ->
+            if(rowA<boardRow-2 && colA<boardCol-2 && board[rowA+2][colA+1]==-2)
+                pathCount++;
+            
+            // |
+            if(rowA<boardRow-2 && board[rowA+1][colA]==-1)
+                pathCount++;
+            
+            if(pathCount>2){
+                board[rowA+1][colA+1]=playerTurn?-3:-4;
+                status=true;
+            }
+            else 
+                pathCount=0;
+
+            
+            // TOP
+
+            //  |
+            // ->
+            if(rowB>1 && board[rowB-1][colB]==-1)
+                pathCount++;
+            
+            // ->
+            // |
+            if(rowA>1 && colA<boardCol-2 && board[rowA-2][colA+1]==-2)
+                pathCount++;
+            
+            // |
+            if(rowA>1 && board[rowA-1][colA]==-1)
+                pathCount++;
+
+            if(pathCount>2){
+                board[rowA-1][colA+1]=playerTurn?-3:-4;
+                status=true;
+            }
+
+        }
+
+        else if(direction==3){
+
+            // left 
+
+            // BOTTOM 
+
+            // <-
+            // |
+            if(rowB<boardRow-2 && board[rowA+1][colA]==-1)
+                pathCount++;
+            
+            //  |
+            // <-
+            if(rowA<boardRow-2 && colA>1 && board[rowA+2][colA-1]==-2)
+                pathCount++;
         
-        if(pathCount==3)
-            return true;
-        return false;
+            // | 
+            if(rowA<boardRow-2 && board[rowA+1][colA]==-1)
+                pathCount++;
+
+            if(pathCount>2){
+                board[rowA+1][colA-1]=playerTurn?-3:-4;
+                status=true;
+            }
+            else 
+                pathCount=0;
+    
+            
+            // RIGHT
+
+            // |
+            // <-
+            if(rowB>1 && board[rowB-1][colB]==-1)
+                pathCount++;
+
+            // <-
+            //  |
+            if(rowA>1 && colA>1 && board[rowA-2][colA-1]==-2)
+                pathCount++;
+            
+            // |
+            if(rowA>1 && board[rowA-1][colA]==-1)
+                pathCount++;
+            
+            if(pathCount>2){
+                board[rowA-1][colA-1]=playerTurn?-3:-4;
+                status=true;
+            }
+
+        }
+
+        return status;
+        
     }
     
     static void playGame(){
@@ -141,6 +295,11 @@ public class Main
             playGame();
         }
         
+        if(pointA>row*col || pointB>row*col){
+            System.out.println("Out of Board! Try again");
+            playGame();
+        }
+
         int rowA=-1,colA=-1,rowB=-1,colB=-1;
         
         int gotPoints=-2;
@@ -178,8 +337,8 @@ public class Main
         }
         
         drawConnection(direction,rowA,colA);
-        boolean connectionIsPresent = checkBox(direction,rowA,colA,rowB,colB);
-        if(connectionIsPresent){
+        boolean boxIsPresent = checkBox(direction,rowA,colA,rowB,colB);
+        if(boxIsPresent){
             if(playerTurn)
                 playerAPoints++;
             else    
@@ -215,9 +374,6 @@ public class Main
 	        }
 	    }
 	    playGame();
-	    
-
-	    
-	
+		
 	}
 }
